0e432a5a1fec54281097b5e67e49fbe574a63863
SakkyOP <saksham298@gmail.com>
2023-03-12 16:53:30 +0530

    Initial Commit added /api/v1/validate/{public_key} endpoint for validating public keys

---
+++ pages/api/v1/validate/[public_key].js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export default function handler(req, res) {
+    const { query } = req;
+    const { public_key } = query;
+
+    return axios.post(
+        "https://rest.cryptoapis.io/blockchain-tools/bitcoin/testnet/addresses/validate",
+        {
+            context: "Public Key Validation",
+            data: {
+                item: {
+                    address: public_key
+                }
+            }
+        },
+        {
+            headers: {
+                "Content-type": "application/json",
+                "X-API-Key": "688313015257ef3866ae0077c42a682cc3b48ed7"
+            }
+        }
+    ).then((response)=> { 
+        return res.status(200).json(response.data);
+    }).catch((error)=> { 
+        return res.status(error.response.status).json(error.response.data); 
+    })
+}
