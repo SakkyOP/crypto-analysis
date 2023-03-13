b1a30e23d56ce5b513d5c20ff6cb911ebfc2a7bd
SakkyOP <saksham298@gmail.com>
2023-03-13 15:23:22 +0530

    updated /api/v1/validate API endpoint Added some UI for test purposes

--- pages/api/v1/validate/[public_key].tsx
+++ pages/api/v1/validate/[public_key].tsx
@@ -1,12 +1,25 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
+export interface validationResponse {
+    data: {
+        item: {
+            address: string,
+            isValid: boolean
+        }
+    }
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { query } = req;
+    if (req.method !== 'POST') {
+        res.status(405).send({ message: 'Only POST requests allowed' });
+        return;
+    }
+    const { query, body } = req;
     const { public_key } = query;
 
     return axios.post(
-        "https://rest.cryptoapis.io/blockchain-tools/bitcoin/testnet/addresses/validate",
+        `https://rest.cryptoapis.io/blockchain-tools/${body.blockchain}/testnet/addresses/validate`,
         {
             context: "Public Key Validation",
             data: {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,34 @@
-
+import React from "react"
+import axios from "axios"
+import useSWR from "swr"
+import { Input, Button } from "@mui/material"
+import { validationResponse } from "./api/v1/validate/[public_key]"
 
 export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
+	const [shouldFetch, setShouldFetch] = React.useState<boolean>(false);
+	const [public_key, setPublicKey] = React.useState<string>("");
+
+	const  fetcher= async (url: string) => {
+		const response: validationResponse = await axios.post(url, {
+			blockchain: "bitcoin"
+		}).then((res)=>{return res.data})
+
+		return response;
+	}
+
+	const {data, error, isLoading} = useSWR(shouldFetch? `api/v1/validate/${public_key}` : null, fetcher)
+
+	// if (isLoading) {
+	// 	setShouldFetch(false);
+	// }
+
+	return (
+		<div>
+			{isLoading? <div> <p> Loading... Please Wait </p> </div> : null}
+			{error? <div> <p> {error.message} </p> </div> : null}
+			{data && data ? <h1>{data.data.item.isValid? "Yes" : "Nope"}</h1> : null}
+			<Input placeholder="Enter Public Key Here" value={public_key} onChange={(event: React.ChangeEvent<HTMLTextAreaElement>)=>setPublicKey(event.target.value)} required/>
+			<Button onClick={()=>{setShouldFetch(true)}}>Check</Button>
+		</div>
+	)
 }
