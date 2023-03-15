631039da22cbf51696de3417f41bfe21ce1130f7
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2023-03-15 22:52:41 +0530

    The first step in UI implementation.

---
+++ component/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+type Props = {}
+
+const LandingPage = (props: Props) => {
+  return (
+    <Fragment>
+        HELLO BSDK!
+    </Fragment>
+  )
+}
+
+export default LandingPage
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Text from '../component/Text'
 import Scan from '../component/Scan'
+import LandingPage from '../component/LandingPage'
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <>
+    <Fragment>
+      <LandingPage/>
       Home
       <Text />
       <Scan />
-    </>   
+    </Fragment>   
   )
 }
 
