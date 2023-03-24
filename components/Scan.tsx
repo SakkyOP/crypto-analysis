7d3111d31f3d9b01c2dca84a00b07c2073fd739b
SakkyOP <saksham298@gmail.com>
2023-03-24 12:17:12 +0530

    refactoring the code

--- components/Scan.tsx
+++ components/Scan.tsx
@@ -1,84 +1,101 @@
 import React, { useState, useRef } from "react";
 import { QrReader, QrReaderProps } from "react-qr-reader";
-import decodeQRCodeFromImage from 'jsqr';
-import { decode, encode } from 'bs58';
-import { createHash } from 'crypto';
+import decodeQRCodeFromImage from "jsqr";
+import { decode, encode } from "bs58";
+import { createHash } from "crypto";
 
 const Scan: React.FC = () => {
-  const [data, setData] = useState<string>('No result');
-  const [input, setInput] = useState<string>('');
-  const [isDecoded, setIsDecoded] = useState<boolean>(false);
-  const fileInputRef = useRef<HTMLInputElement>(null);
+	const [data, setData] = useState<string>("No result");
+	const [isDecoded, setIsDecoded] = useState<boolean>(false);
+	const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleResult = (result: any, error: any) => {
-    if (!!result) {
-      decodeData(result.text);
-    }
+	const handleResult = (result: any, error: any) => {
+		if (!!result) {
+			decodeData(result.text);
+		}
 
-    if (!!error) {
-      console.info(error.message);
-    }
-  };
+		if (!!error) {
+			console.info(error.message);
+		}
+	};
 
-  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        const image = new Image();
-        image.src = reader.result as string;
-        image.onload = () => {
-          const canvas = document.createElement('canvas');
-          const ctx = canvas.getContext('2d');
-          canvas.width = image.width;
-          canvas.height = image.height;
-          ctx?.drawImage(image, 0, 0);
-          const imageData = ctx?.getImageData(0, 0, canvas.width, canvas.height);
-          const qrCode = decodeQRCodeFromImage(imageData!.data, imageData!.width, imageData!.height);
-          if (qrCode) {
-            decodeData(qrCode.data);
-          } else {
-            console.info('QR code not found');
-          }
-        };
-      };
-    }
-  };
+	const handleFileInputChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		const file = event.target.files?.[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = () => {
+				const image = new Image();
+				image.src = reader.result as string;
+				image.onload = () => {
+					const canvas = document.createElement("canvas");
+					const ctx = canvas.getContext("2d");
+					canvas.width = image.width;
+					canvas.height = image.height;
+					ctx?.drawImage(image, 0, 0);
+					const imageData = ctx?.getImageData(
+						0,
+						0,
+						canvas.width,
+						canvas.height
+					);
+					const qrCode = decodeQRCodeFromImage(
+						imageData!.data,
+						imageData!.width,
+						imageData!.height
+					);
+					if (qrCode) {
+						decodeData(qrCode.data);
+					} else {
+						console.info("QR code not found");
+					}
+				};
+			};
+		}
+	};
 
-  const decodeData = (data: string) => {
-    try {
-      const decodedData = decode(data);
-      const checksum = createHash('sha256').update(decodedData).digest().slice(0, 4);
-      const encodedData = encode(Buffer.concat([decodedData, checksum]));
-      setData(encodedData);
-      setIsDecoded(true);
-    } catch {
-      setData(data);
-      setIsDecoded(false);
-    }
-  };
+	const decodeData = (data: string) => {
+		try {
+			const decodedData = decode(data);
+			const checksum = createHash("sha256")
+				.update(decodedData)
+				.digest()
+				.slice(0, 4);
+			const encodedData = encode(Buffer.concat([decodedData, checksum]));
+			setData(encodedData);
+			setIsDecoded(true);
+		} catch {
+			setData(data);
+			setIsDecoded(false);
+		}
+	};
 
-  const qrReaderProps: QrReaderProps = {
-    onResult: handleResult,
-    constraints: {
-      facingMode: 'environment',
-    },
-  };
+	const qrReaderProps: QrReaderProps = {
+		onResult: handleResult,
+		constraints: {
+			facingMode: "environment",
+		},
+	};
 
-  return (
-    <>
-      <QrReader {...qrReaderProps} />
-      <p>{data}</p>
-      <p>{isDecoded ? "Success" : "Can't decode"}</p>
-      <form>
-        <label>
-          Upload QR code image:
-          <input type="file" ref={fileInputRef} onChange={handleFileInputChange} />
-        </label>
-      </form>
-    </>
-  );
+	return (
+		<>
+			<QrReader {...qrReaderProps} />
+			<p>{data}</p>
+			<p>{isDecoded ? "Success" : "Can't decode"}</p>
+			<form>
+				<label>
+					Upload QR code image:
+					<input
+						type="file"
+						ref={fileInputRef}
+						onChange={handleFileInputChange}
+					/>
+				</label>
+			</form>
+		</>
+	);
 };
 
 export default Scan;
--- components/Text.tsx
+++ components/Text.tsx
@@ -1,35 +1,45 @@
-import React from "react"
-import axios from "axios"
-import useSWR from "swr"
-import { Input, Button, Box, Stack } from "@mui/material"
-import { validationResponse } from "../pages/api/v1/validate/[public_key]"
-import { detectorResponse } from "../pages/api/v1/detector/[address]"
-import { Cryptocon } from "cryptocons"
-import cryptos from './cryptos.json'
-
+import React from "react";
+import axios from "axios";
+import useSWR from "swr";
+import { Input, Button, Box, Stack } from "@mui/material";
+import { validationResponse } from "../pages/api/v1/validate/[public_key]";
+import { detectorResponse } from "../pages/api/v1/detector/[address]";
+import { Cryptocon } from "cryptocons";
+import cryptos from "./cryptos.json";
 
 export default function Text() {
 	const [shouldFetch, setShouldFetch] = React.useState<boolean>(false);
 	const [public_key, setPublicKey] = React.useState<string>("");
-	const [response, setResponse] = React.useState<detectorResponse | null>(null);
+	const [response, setResponse] = React.useState<detectorResponse | null>(
+		null
+	);
 
 	// For CrpytoAPIs api
-	const  fetcher= async (url: string) => {
-		const response: validationResponse = await axios.post(url, {
-			blockchain: "bitcoin",
-			network: "testnet"
-		}).then((res)=>{return res.data})
+	const fetcher = async (url: string) => {
+		const response: validationResponse = await axios
+			.post(url, {
+				blockchain: "bitcoin",
+				network: "testnet",
+			})
+			.then((res) => {
+				return res.data;
+			});
 
 		return response;
-	}
+	};
 
-	const  fetchBlockchain= async (url: string) => {
-		const response: detectorResponse = await axios.get(url).then((res)=>{return res.data})
+	const fetchBlockchain = async (url: string) => {
+		const response: detectorResponse = await axios.get(url).then((res) => {
+			return res.data;
+		});
 
 		return response;
-	}
+	};
 
-	const {data, error, isLoading} = useSWR(shouldFetch? `api/v1/detector/${public_key}` : null, fetchBlockchain)
+	const { data, error, isLoading } = useSWR(
+		shouldFetch ? `api/v1/detector/${public_key}` : null,
+		fetchBlockchain
+	);
 
 	if (data) {
 		setResponse(data);
@@ -39,18 +49,54 @@ export default function Text() {
 		<Box className="flex justify-center items-center h-screen text-white">
 			<Stack className="w-auto flex justify-center items-center">
 				{/* Add Loading component in this:  (Remove <div>... and replace with the loading component) */}
-				{isLoading? <div> <p> Loading... Please Wait </p> </div> : null}
-				
+				{isLoading ? (
+					<div>
+						<p> Loading... Please Wait </p>
+					</div>
+				) : null}
+
 				{/* Add Error component here */}
-				{error? <div> <p> {error.message} </p> </div> : null}
+				{error ? (
+					<div>
+						<p> {error.message} </p>
+					</div>
+				) : null}
 
 				{/* This is the response component return value is mentioned in Docs */}
 				<div className="flex justify-center items-center">
-				{response && response ? <h1 className="flex text-lg">{ response.cryptocurrency.length > 3? null : <span> <Cryptocon size={25} badgeRadius={100} icon={cryptos[response.cryptocurrency]} /> </span> } <p className="pl-2 mb-[2px] pb-[2px]"> { response.cryptocurrency } </p></h1> : null}
+					{response && response ? (
+						<h1 className="flex text-lg">
+							{response.cryptocurrency.length > 3 ? null : (
+								<span>
+									<Cryptocon
+										size={"1.5vw"}
+										badgeRadius={100}
+										icon={cryptos[response.cryptocurrency]}
+									/>
+								</span>
+							)}
+							<p className="pl-2 mb-[2px] pb-[2px]">
+								{response.cryptocurrency}
+							</p>
+						</h1>
+					) : null}
 				</div>
-				<Input className="text-white w-[15rem] placeholder:textwhite" placeholder="Enter Public Key Here" value={public_key} onChange={(event: React.ChangeEvent<HTMLTextAreaElement>)=>setPublicKey(event.target.value)} required/>
-				<Button onClick={()=>{setShouldFetch(true)}}>Check</Button>
+				<Input
+					className="text-white w-[15rem] placeholder:textwhite"
+					placeholder="Enter Public Key Here"
+					value={public_key}
+					onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
+						setPublicKey(event.target.value)
+					}
+					required
+				/>
+				<Button
+					onClick={() => {
+						setShouldFetch(true);
+					}}>
+					Check
+				</Button>
 			</Stack>
 		</Box>
-	)
+	);
 }
