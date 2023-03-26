35cf2c02fb05c4f40c12dbef40c65ec29abdd71f
SakkyOP <saksham298@gmail.com>
2023-03-27 02:37:04 +0530

    added response handle in Scan script

--- components/Scan.tsx
+++ components/Scan.tsx
@@ -1,17 +1,50 @@
 import React, { useState, useRef, Fragment } from "react";
 import { QrReader, QrReaderProps } from "react-qr-reader";
 import decodeQRCodeFromImage from "jsqr";
-import { decode, encode } from "bs58";
-import { createHash } from "crypto";
+import useSWR from 'swr'
+import axios from "axios";
+import { detectorResponse } from "../pages/api/v1/detector/[address]";
+import { Cryptocon, CryptoconProps } from "cryptocons";
+import cryptos from './cryptos.json'
+
+interface detectionStatus {
+	isDetected: boolean,
+	name? : string | "X",
+	icon? : JSX.Element | "X"   // Cryptocon Element
+}
 
 const Scan: React.FC = () => {
-	const [data, setData] = useState<string>("No result");
-	const [isDecoded, setIsDecoded] = useState<boolean>(false);
 	const fileInputRef = useRef<HTMLInputElement>(null);
 
+	const [detection, setDetection] = useState<detectionStatus | undefined>();
+	const [shouldFetch, setShouldFetch] = useState<boolean>(false)
+	const [public_key, setPublic_key] = useState<string>("");
+
+	const fetcher= async (url: string) => {
+		const response: detectorResponse =  await axios.get(url).then(res=>{ return res.data })
+		return response;
+	}
+
+	const { data: res_data , isLoading, error} = useSWR(shouldFetch ? `api/v1/detector/${public_key}` : null, fetcher);
+
+	if (res_data){
+		console.log(res_data);
+		
+		setDetection({
+			isDetected: res_data.cryptocurrency.length < 5,
+			name: res_data.cryptocurrency.length < 5 ? res_data.cryptocurrency : "X",
+			icon: ( ( res_data.cryptocurrency.length < 5 ) && ( Object.keys( cryptos ).includes( res_data.cryptocurrency ) ) ) ? <Cryptocon 
+					size={"3rem"}
+					badgeRadius={100}
+					icon={cryptos[res_data.cryptocurrency]}
+				/> : "X"
+		});
+		setShouldFetch(false);
+	}
+	
 	const handleResult = (result: any, error: any) => {
 		if (!!result) {
-			decodeData(result.text);
+			handleData(result.text);
 		}
 
 		if (!!error) {
@@ -47,7 +80,7 @@ const Scan: React.FC = () => {
 						imageData!.height
 					);
 					if (qrCode) {
-						decodeData(qrCode.data);
+						handleData(qrCode.data);
 					} else {
 						console.info("QR code not found");
 					}
@@ -56,20 +89,9 @@ const Scan: React.FC = () => {
 		}
 	};
 
-	const decodeData = (data: string) => {
-		try {
-			const decodedData = decode(data);
-			const checksum = createHash("sha256")
-				.update(decodedData)
-				.digest()
-				.slice(0, 4);
-			const encodedData = encode(Buffer.concat([decodedData, checksum]));
-			setData(encodedData);
-			setIsDecoded(true);
-		} catch {
-			setData(data);
-			setIsDecoded(false);
-		}
+	const handleData = (data: string) => {
+		setPublic_key( data );
+		setShouldFetch( true );
 	};
 
 	const qrReaderProps: QrReaderProps = {
@@ -82,11 +104,10 @@ const Scan: React.FC = () => {
 	return (
 		<Fragment>
 			{/* FIX SIZE and MAKE IT PRESENTABLE */}
+			{/* Display only when opted using Camera Button */}
 			<div className="w-40 h-20">
-			<QrReader {...qrReaderProps} />
+				<QrReader {...qrReaderProps} />
 			</div>
-			<p>{data}</p>
-			<p>{isDecoded ? "Success" : "Can't decode"}</p>
 			<form>
 				<label>
 					Upload QR code image:
@@ -97,8 +118,20 @@ const Scan: React.FC = () => {
 					/>
 				</label>
 			</form>
+
+			<div className="mt-[-1rem] px-20 max-md:px-10">
+				<div className="text-[1.6rem] mb-2">Discription:</div>
+				<div className=" w-[28rem] max-md:w-[20rem] px-6 py-4 h-[16rem] border-2 rounded-md">
+					<div className="flex p-2 text-[1.4rem]">
+						<p className="mr-2">Status:</p> { detection && detection ? (
+							<p className={`${ detection.isDetected ? "text-green-400" : "text-red-600" }`}> { detection.isDetected ? "Detected" : "Not Detected"} </p>
+						) : null }
+					</div>
+					<div className="flex p-2 text-[1.4rem]"><p className="mr-2">Crypto Icon:</p><div className="mt-[-0.4rem]">{ detection && detection.icon }</div> </div>
+					<div className="flex p-2 text-[1.4rem]"><p className="mr-2">Crypto Name:</p><p>{ detection && detection.name }</p></div>
+				</div>
+			</div>
 		</Fragment>
 	);
-};
-
+}
 export default Scan;
