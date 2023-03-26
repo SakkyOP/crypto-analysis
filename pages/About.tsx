23af490ec95f225cd0b6b53ec4758b378557c2e1
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2023-03-26 21:30:14 +0530

    Working On about Page

---
+++ pages/About.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+type Props = {}
+
+const About = (props: Props) => {
+  return (
+    <Fragment>
+        <div className='text-white'>
+            About Page
+        </div>
+    </Fragment>
+  )
+}
+
+export default About
